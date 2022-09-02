d845265a30916ecc04871c1e1149ecbc1b5daa85
florent guilbaud <florent.guilbaud@gmail.com>
2022-09-02 09:53:40 +0200

    adding form test

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,27 @@
 class ProductsController < ApplicationController
-  skip_before_action :authenticate_user!, only: :index
+  skip_before_action :authenticate_user!, only: %i[index new]
 
   def index
     @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to root_path
+    else
+      render :new, status: unprocessable_entity
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :tagline)
   end
 end
